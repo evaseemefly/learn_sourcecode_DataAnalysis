ea189d38154c7f5ef80891fd4a48e91898512cb3
liusihan-单位 <evaseemefly@126.com>
2017-12-20 16:59:47 +0800

    完成部分内容

---
+++ codes_bymyself/main.py
@@ -0,0 +1,40 @@
+import re
+import os
+
+files=[]
+def getmatchingFiles(path):
+    # 当前路径下的目录
+    year='2017'
+    month='11'    
+    start='01'
+    end='30'
+    source_dir=os.listdir(path)
+    if year in source_dir:
+        year_dir=os.path.join(path,'2017')
+        month_dir=os.listdir(year_dir)        
+        if month in month_dir:
+            targetpath=os.path.join(year_dir,month)
+            for root, dirs, files in os.walk(targetpath): 
+                print("-----------")
+                print("root:%s"%root)
+                print("dirs:%s"%dirs)
+                print("files:%s"%files)
+                
+#     for temp in source_dir:
+#          # 找到指定年份
+#         # 判断是否为年份
+#         # 四位数字
+#         res=re.match(r"\d{4}",source_dir)
+        
+#     print(source_dir)
+#     if len(source_dir)==1:    
+       
+#         for year in res:
+            
+#         if res!=None:
+#             # 若是四位数字再
+#             print("匹配")
+#         print(res)
+
+path=r'E:\03协同开发\99学习\05数据分析\网课源码\learn_sourcecode_DataAnalysis\codes_bymyself\data\sanya\perclock'
+getmatchingFiles(path)
